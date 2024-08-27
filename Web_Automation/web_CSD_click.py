7ad3e80762a65304d6db778552c4bc4e0d600f1a
Enes Can Türkoğlu <enescan58turko@gmail.com>
2024-08-28 00:15:46 +0300

    v0.0.2
    - Facebook'a asagida verilen username ve password'u vererek giris yapan script yazildi

--- Web_Automation/web_CSD_click.py
+++ Web_Automation/web_CSD_click.py
@@ -1,20 +1,30 @@
-import requests                 # Requests HTTP Library
-from bs4 import BeautifulSoup   # bs4 içerisindeki BeautifulSoup class'ını içeri aktarır
-from selenium import webdriver
+# ---
+# Facebook'a asagida verilen username ve password'u vererek giris yapan script
 # ---
 
-# GeckoDriver'ın dosya yolunu belirtin
-driver_path = 'C:/Users/enesc/Masaüstü/Python/Web_Test/eklenti'
-# "C:/Users/enesc/Masaüstü/Python/Web_Test/eklenti/geckodriver.exe"
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# ---
+
+username = "enescan58turko@hotmail.com"
+password = "XYZ"
+
 
 # Firefox WebDriver'ı başlat
 my_web_driver = webdriver.Firefox()
-my_web_driver.get('https://csystem.org/')
+my_web_driver.get('https://www.facebook.com/?locale=tr_TR')
+
+
+my_web_driver.find_element(By.NAME, "email").send_keys(username)
+my_web_driver.find_element(By.NAME, "pass").send_keys(password)
+my_web_driver.find_element(By.NAME, "login").click()
 
 
 
 
-
+# --- KAYNAKCA
+# https://www.youtube.com/watch?v=doPo9q6on6c&t=206s
 
 
 # from selenium import webdriver
